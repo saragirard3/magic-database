5fd7443cb9c00f5468fb385f755290e2dc4e34fb
Sara Girard <sara@paditim.com>
2026-03-09 10:22:26 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "REACT_APP_FIREBASE_API_KEY",
+  authDomain: "REACT_APP_FIREBASE_AUTH_DOMAIN",
+  projectId: "REACT_APP_FIREBASE_PROJECT_ID",
+  storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "REACT_APP_FIREBASE_MESSAGING_SENDER_ID",
+  appId: "REACT_APP_FIREBASE_APP_ID",
+  databaseURL: "REACT_APP_FIREBASE_DATABASE_URL"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// https://medium.com/@marvellousnujebor/how-to-create-a-simple-crud-application-with-react-and-firebase-ed8dcf738478
